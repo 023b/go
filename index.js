efb8bd7f539cbcd6fbd69d362aa787b335eeb274
023b <arunsabapathi2486@gmail.com>
2024-04-01 00:00:00 +0530

    Commit on 2024-04-01T00:00:00+05:30

--- index.js
+++ index.js
@@ -1,52 +1,29 @@
-const FILE_PATH = "./data.json";
 const simpleGit = require("simple-git");
 const jsonfile = require("jsonfile");
 const moment = require("moment");
 const random = require("random");
+const FILE_PATH = "./data.json";
+const git = simpleGit();
 
-// Change the working directory to where your local repository is located
-const git = simpleGit("/Users/rohitaggarwal/Desktop/test/GitHub_Graph");
+const makeCommit = async (startDate, endDate, commitsPerDay = 5) => {
+  const current = moment(startDate);
+  const end = moment(endDate);
 
-const makeCommit = (n) => {
-  if (n === 0) {
-    // Push changes to the remote repository
-    git.push(["-u", "origin", "master"], (err, result) => {
-      if (err) {
-        console.error("Error pushing to remote:", err);
-      } else {
-        console.log("Pushed changes to remote repository");
-      }
-    });
-    return;
+  while (current.isBefore(end)) {
+    const date = current.format();
+    const data = { date };
+    await jsonfile.writeFile(FILE_PATH, data);
+
+    for (let i = 0; i < commitsPerDay; i++) {
+      await git.add(FILE_PATH);
+      await git.commit(`Commit on ${date}`, { "--date": date });
+    }
+
+    current.add(1, "day");
   }
 
-  const x = random.int(0, 54);
-  const y = random.int(0, 6);
-  const DATE = moment()
-    .subtract(0, "y")
-    .add(1, "d")
-    .add(x, "w")
-    .add(y, "d")
-    .format();
-
-  const data = {
-    date: DATE,
-  };
-  console.log(DATE);
-
-  jsonfile.writeFile(FILE_PATH, data, () => {
-    git
-      .add([FILE_PATH])
-      .commit(DATE, { "--date": DATE })
-      .push(["-u", "origin", "origin"], (err, result) => {
-        if (err) {
-          console.error("Error pushing to remote:", err);
-        } else {
-          console.log("Pushed changes to remote repository");
-          makeCommit(--n);
-        }
-      });
-  });
+  await git.push();
+  console.log("Pushed all commits!");
 };
 
-makeCommit(120);
+makeCommit("2024-04-01", moment().format("YYYY-MM-DD"));
